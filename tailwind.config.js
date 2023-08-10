6e6d0762b156c4dacf88980b6c08cb987fb87bf5
Irina Konoshenko <ayren.syssoyeva@gmail.com>
2023-08-10 20:09:24 +0600

    second commit

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,11 @@
 module.exports = {
   content: ["./dist/*.html"],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        accent: "#E58411",
+      },
+    },
   },
   plugins: [],
 };
